1aed379468275920267842ba7f868764d9ca9b1c
NIODER <nioder125@gmail.com>
2023-06-09 20:52:33 +0300

    vt

--- run.bat
+++ run.bat
@@ -1,1 +1,37 @@
 python entropy.py -f text.txt
+
+python entropy.py -f ".\files\aaa.txt" >> output.txt
+python entropy.py -f ".\files\alphabet.txt" >> output.txt
+python entropy.py -f ".\files\fireworks.jpeg" >> output.txt
+python entropy.py -f ".\files\geo.protodata" >> output.txt
+python entropy.py -f ".\files\html" >> output.txt
+python entropy.py -f ".\files\kennedy.xls" >> output.txt
+python entropy.py -f ".\files\paper-100k.pdf" >> output.txt
+python entropy.py -f ".\files\pi.txt" >> output.txt
+python entropy.py -f ".\files\random.txt" >> output.txt
+python entropy.py -f ".\files\sum" >> output.txt
+python entropy.py -f ".\files\tls_client" >> output.txt
+python entropy.py -f ".\files\urls.10K" >> output.txt
+python entropy.py -f ".\files\xml" >> output.txt
+python entropy.py -f ".\files\xml.rar" >> output.txt
+python entropy.py -f ".\files\xml.zip" >> output.txt
+python entropy.py -f ".\files\xml2.rar" >> output.txt
+
+
+@REM name            entropy
+@REM aaa.txt         0.0
+@REM alphabet.txt    4.700439711793615
+@REM fireworks.jpeg  7.974554177833788
+@REM geo.protodata   7.062731879320851
+@REM html            5.200188138857744
+@REM kennedy.xls     3.573470858581421
+@REM paper-100k.pdf  7.590197979816022
+@REM pi.txt          3.321924113075452
+@REM random.txt      5.9994884002800815
+@REM sum             5.328990058101005
+@REM tls_client      7.389526484174756
+@REM urls.10K        5.257097944882001
+@REM xml             5.518154617082
+@REM xml.rar         7.999356916931891
+@REM xml.zip         7.999429719357024
+@REM xml2.rar        7.998897192821083
